ff9f5b62ed8c08fad2040762ebb33ef2fea92290
Adam26114 <zakato26114@gmail.com>
2023-05-25 07:48:25 +0630

    25 May 2023

--- app.js
+++ app.js
@@ -24,43 +24,43 @@ const ctx = document.getElementById('mypiechart');
 ctx.height = 240;
   new Chart(ctx, {
     type: 'doughnut',
-        data: {
-        
-            datasets: [{
-                data: [12, 19, 3],
+    data: {
+      
+      datasets: [{
+        data: [12, 19, 3],
 
-            }]
-        },
-        options: {
-            responsive:false
-        }
+      }]
+    },
+    options: {
+      responsive:false
+    }
   });
 
 // End Expenses Area
 
 // Start Earning Area
-google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+ google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
+      function drawChart() {
         var data = google.visualization.arrayToDataTable([
-            ['Year', 'Sales', 'Expenses'],
-            ['2004',  1000,      400],
-            ['2005',  1170,      460],
-            ['2006',  660,       1120],
-            ['2007',  1030,      540]
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
         ]);
 
         var options = {
-            title: 'Company Performance',
-            curveType: 'function',
-            legend: { position: 'bottom' }
+          title: 'Company Performance',
+          curveType: 'function',
+          legend: { position: 'bottom' }
         };
 
         var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 
         chart.draw(data, options);
-    }
+      }
 // End Earning ARea
 
 // End Js Area
